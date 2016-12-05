e9de79edaaab9fc36f740145f0ff36fefcb9a572
Daniel Esteban Nombela <dani@gatunes.com>
2016-12-05 08:40:01 +0100

    more refactoring and piwik experiment

--- server.js
+++ server.js
@@ -17,6 +17,59 @@ const LedEvent = (led) => (
 	led.id + ',' + led.color.r + ',' + led.color.g + ',' + led.color.b + ',' + led.mode
 );
 
+/* UpdateLed Helper */
+const UpdateLed = (id, r, g, b, mode) => {
+	for(let i=0; i<LEDS.length; i++) {
+		const led = LEDS[i];
+		if(led.id === id) {
+			led.color = {r, g, b};
+			led.mode = mode;
+			events.initial.length = 0;
+			for(let i=0; i<LEDS.length; i++) events.initial.push(LedEvent(LEDS[i]));
+			led.send(new Buffer([led.color.r, led.color.g, led.color.b, led.mode]), () => {});
+			events.send(LedEvent(led), 'update');
+			return true;
+		}
+	}
+};
+
+/* Exprees config & routes */
+production && app.use(helmet());
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.set('trust proxy', 'loopback');
+app.get('/events', events.init);
+app.post('/led', (req, res) => {
+	req.checkBody('id').notEmpty().isInt();
+	req.checkBody('r').notEmpty().isInt();
+	req.checkBody('g').notEmpty().isInt();
+	req.checkBody('b').notEmpty().isInt();
+	req.checkBody('mode').notEmpty().isInt();
+	req.getValidationResult().then((result) => {
+    if(!result.isEmpty()) return res.send('FAIL', 400);
+		res.send(UpdateLed(req.body.id, req.body.r, req.body.g, req.body.b) ? 'OK' : 'FAIL');
+	});
+});
+app.post('/piwik', (req, res) => {
+	req.checkBody('id').notEmpty().isInt();
+	req.getValidationResult().then((result) => {
+		if(!result.isEmpty()) return res.sendStatus(400).end();
+
+		/* SUPER HACKY ALERT !!! */
+		const ledID = 14150704;
+		if(req.body.id === 1) {
+			// dani.gatunes.com
+			UpdateLed(ledID, 0, 0, 255, 1);
+		}
+		if(req.body.id === 2) {
+			// voxels.es
+			UpdateLed(ledID, 255, 0, 0, 1);
+		}
+
+		res.end();
+	});
+});
+
 /* LedNet server */
 WS.on('connection', (led) => {
 	led.id = parseInt(led.upgradeReq.url.substr(1), 10);
@@ -47,38 +100,6 @@ WS.on('connection', (led) => {
 	events.send(LedEvent(led), 'add');
 	led.send(new Buffer([led.color.r, led.color.g, led.color.b, led.mode]), () => {});
 });
-
-/* Exprees config & routes */
-production && app.use(helmet());
-app.use(bodyParser.json());
-app.use(expressValidator());
-app.set('trust proxy', 'loopback');
-app.post("/led", (req, res) => {
-	req.checkBody('id').notEmpty().isInt();
-	req.checkBody('r').notEmpty().isInt();
-	req.checkBody('g').notEmpty().isInt();
-	req.checkBody('b').notEmpty().isInt();
-	req.checkBody('mode').notEmpty().isInt();
-	req.getValidationResult().then((result) => {
-    if(!result.isEmpty()) return res.send("FAIL", 400);
-		for(let i=0; i<LEDS.length; i++) {
-			const led = LEDS[i];
-			if(led.id === req.body.id) {
-				events.initial.length = 0;
-				led.color = {r: req.body.r, g: req.body.g, b: req.body.b};
-				led.mode = req.body.mode;
-				for(let i=0; i<LEDS.length; i++) events.initial.push(LedEvent(LEDS[i]));
-				led.send(new Buffer([led.color.r, led.color.g, led.color.b, led.mode]), () => {});
-				events.send(LedEvent(led), 'update');
-				return res.send("OK");
-			}
-			res.send("FAIL");
-		}
-	});
-});
-
-/* Event source */
-app.get('/events', events.init);
 
 /* App server */
 if(production) {
