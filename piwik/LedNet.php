14d1a6987a024e75c92a0dc41b22db0cb56e156d
Daniel Esteban Nombela <dani@gatunes.com>
2016-12-08 01:22:28 +0100

    Avoid event tracking hits on piwik plugin

--- piwik/LedNet.php
+++ piwik/LedNet.php
@@ -2,18 +2,24 @@
 namespace Piwik\Plugins\LedNet;
 
 class LedNet extends \Piwik\Plugin {
+	const SERVER_URL = 'https://projects.gatunes.com/lednet/';
+
 	public function registerEvents() {
 		return array(
 			'Tracker.Request.getIdSite' => 'trackVisit'
 		);
 	}
 
-	/* Notify the LedNet server on every pageview */
-	public function trackVisit(&$idSite, $params) {
-		if(!empty($params['link'])) return;
-		$req = curl_init("https://projects.gatunes.com/lednet/piwik");
+	public function trackVisit($idSite, $params) {
+		if(!empty($params['link']) || !empty($params['e_c']) {
+			/* Avoid outlinks & events */
+			return;
+		}
+
+		/* Notify the LedNet server on every pageview */
+		$req = curl_init(self::SERVER_URL . 'piwik');
 		$payload = json_encode(array(
-			"id" => $idSite
+			'id' => $idSite
 		));
 		curl_setopt($req, CURLOPT_POSTFIELDS, $payload);
 		curl_setopt($req, CURLOPT_HTTPHEADER, array(
